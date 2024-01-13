98b10b772a10d3821e0408c0e6ccf6ee261bc7f5
Math92 <mathiassan92@gmail.com>
2024-01-12 21:32:53 -0300

    modifying project wireframe and files

--- README.md
+++ README.md
@@ -1,25 +1,17 @@
-# 🎬 Buscador de Películas
+# Day #11
 
-Este proyecto es un buscador de películas que permite a los usuarios buscar películas por título utilizando la API de [The Movie Database (TMDb)](https://www.themoviedb.org/). 
-Es una aplicación web sencilla pero potente, diseñada para proporcionar información rápida y precisa sobre películas.
+### Movie Info App
+In this tutorial ([Open in Youtube](https://youtu.be/J6uam9jEmDU)),  I am gonna showing to you how to code a movie info app with javascript. in this tutorial also we use a movie database api and we get data from api and this code is also responsive❗️
 
-## 🌟 Características
+## Warning
+You need to get your own api key (in video we showed how!) and replace it in key.js file :
 
-- **Búsqueda de Películas**: Los usuarios pueden buscar películas por título.
-- **Resultados Paginados**: Los resultados de la búsqueda se muestran de manera paginada para una mejor navegación.
-- **Información Detallada**: Cada película incluye detalles como el título, la fecha de lanzamiento y una breve descripción.
+```javascript
+key = "Your Api Key";
+```
 
-## 🛠 Tecnologías Utilizadas
 
-- **HTML**: Estructura de la página web.
-- **CSS**: Estilos para hacer la interfaz de usuario más atractiva.
-- **JavaScript**: Funcionalidad para realizar solicitudes a la API y manipular el DOM.
-- **Bootstrap**: Framework CSS para un diseño responsivo y estilizado.
+# Screenshot
+Here we have project screenshot :
 
-## ⚙️ Configuración y Ejecución
-
-Para ejecutar este proyecto en tu máquina local, sigue estos pasos:
-
-1. Clona el repositorio en tu máquina local.
-2. Abre el archivo `index.html` en un navegador web para ver la aplicación.
-3. Para realizar búsquedas, escribe un título de película en el campo de búsqueda y presiona el botón "Buscar".
+![screenshot](screenshot.jpg)
---
+++ index.js
@@ -0,0 +1,115 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    let movieNameRef = document.getElementById("movie-name");
+    let searchBtn = document.getElementById("search-btn");
+    let result = document.getElementById("result");
+    let pagination = document.getElementById("pagination");
+    let currentPage = 1;
+    let totalResults = 0;
+    const resultsPerPage = 10; // Assuming 10 results per page
+    const maxVisiblePages = 5; // Maximum number of visible pages at once
+
+    let getMovie = (page) => {
+        result.innerHTML = `<h3 class="loading-message">Loading movies...</h3>`;
+        result.classList.add('fadeIn');
+        let movieName = movieNameRef.value.trim();
+        let url = `https://www.omdbapi.com/?s=${encodeURIComponent(movieName)}&apikey=${key}&page=${page}`;
+
+        if (movieName.length <= 0) {
+            result.innerHTML = `<h3 class="msg">Please enter a movie name.</h3>`;
+            pagination.innerHTML = '';
+        } else {
+            fetch(url).then((resp) => resp.json()).then((data) => {
+                if (data.Response == "True") {
+                    totalResults = parseInt(data.totalResults);
+                    let movieDetailsPromises = data.Search.map(movie => fetch(`https://www.omdbapi.com/?i=${movie.imdbID}&apikey=${key}`)
+                        .then(response => response.json()));
+
+                    Promise.all(movieDetailsPromises).then(details => {
+                        result.innerHTML = details.map(detail => `
+                            <div class="info">
+                                <img src="${detail.Poster}" class="poster">
+                                <div class="info-movie">
+                                    <h2>${detail.Title}</h2>
+                                    <div class="rating">
+                                        <img src="star-icon.svg" alt="Star icon">
+                                        <h4>${detail.imdbRating}</h4>
+                                    </div>
+                                    <div class="details">
+                                        <span>${detail.Rated}</span>
+                                        <span>${detail.Year}</span>
+                                        <span>${detail.Runtime}</span>
+                                    </div>
+                                    <div class="genre">${detail.Genre.split(',').map(genre => `<div>${genre.trim()}</div>`).join('')}</div>
+                                    <h3>Plot:</h3>
+                                    <p>${detail.Plot}</p>
+                                    <h3>Cast:</h3>
+                                    <p>${detail.Actors}</p>
+                                </div>
+                            </div>
+                        `).join('');
+                        setupPagination(totalResults);
+                        result.classList.add('fadeIn');
+
+                    });
+                } else {
+                    result.innerHTML = `<h3 class="msg">${data.Error}</h3>`;
+                    pagination.innerHTML = '';
+                }
+            })
+                .catch((error) => {
+                    result.innerHTML = `<h3 class="msg">Error Occurred: ${error.message}</h3>`;
+                    pagination.innerHTML = '';
+                });
+        }
+    };
+
+    function setupPagination(totalMovies) {
+        const pageCount = Math.ceil(totalMovies / resultsPerPage);
+        pagination.innerHTML = '';
+
+        let startPage = Math.max(currentPage - Math.floor(maxVisiblePages / 2), 1);
+        let endPage = Math.min(startPage + maxVisiblePages - 1, pageCount);
+
+        if (endPage - startPage < maxVisiblePages - 1) {
+            startPage = Math.max(endPage - maxVisiblePages + 1, 1);
+        }
+
+        if (startPage > 1) {
+            addPaginationItem(pagination, 1, '<<');
+        }
+
+        if (currentPage > 1) {
+            addPaginationItem(pagination, currentPage - 1, '<');
+        }
+
+        for (let i = startPage; i <= endPage; i++) {
+            addPaginationItem(pagination, i, i);
+        }
+
+        if (currentPage < pageCount) {
+            addPaginationItem(pagination, currentPage + 1, '>');
+        }
+
+        if (endPage < pageCount) {
+            addPaginationItem(pagination, pageCount, '>>');
+        }
+    }
+
+    function addPaginationItem(container, page, text) {
+        const button = document.createElement('button');
+        button.textContent = text;
+        button.addEventListener('click', function (e) {
+            e.preventDefault();
+            currentPage = page;
+            getMovie(currentPage);
+        });
+
+        if (page === currentPage) {
+            button.classList.add('active'); // Add an 'active' class or disable the button
+        }
+
+        container.appendChild(button);
+    }
+
+    searchBtn.addEventListener("click", () => getMovie(currentPage));
+});
